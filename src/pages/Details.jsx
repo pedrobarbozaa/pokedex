11f4c0cf6ac34e8296253fcfa31c30e1304047f2
Pedro de Andrade <pedrodeandrade2@gmail.com>
2023-04-26 19:51:27 -0300

    Começando a página de detalhes

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 // import "./style.css";
+import { fetchPoke } from "../services/apiRequests"
 
 class Details extends React.Component {
+  state = {
+    data: {},
+    loading: false,
+    name: '',
+    image: '',
+    types: [],
+  }
+  
+  componentDidMount() {
+    const { match: { params: {id} } } = this.props
+    this.populateState(id);
+  }
+
+  populateState = async (id) => {
+    const data = await fetchPoke(id)
+    this.setState({
+      data,
+      name: data.name,
+      image: data.sprites.other["official-artwork"].front_default,
+      type: data.types.map((el) => el.type.name),
+      loading: false,
+    })
+  }
+
   render() {
-    const { name, image, type, save, className } = this.props
+    const { loading, name, image, type, data} = this.state
+    console.log(data.sprites);
+
     return (
-      <div onClick={save} className={className}>
-        {/* <p className="name">{ name }</p>
+      <div>
+        <p className="name">{ name }</p>
         <img className="img" src={ image } alt={ `Imagem ${name}` } />
-        <p className="type">{ type }</p> */}
-        <p>TESTE</p>
+        <p className="type">{ type?.[0].toUpperCase() }</p>
+        <p className="type">{ type?.[1].toUpperCase() }</p>
       </div>
     )
   }
